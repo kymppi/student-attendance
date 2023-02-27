52344f66b0e826517562bd785d7c5fa0a502c4cf
Midka <me@midka.dev>
2023-02-27 20:25:43 +0200

    add: google oauth2

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import prisma from '../../../lib/prisma-client';
+
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error('GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET must be set');
+}
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
